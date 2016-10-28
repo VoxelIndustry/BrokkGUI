f27d6c0d7b62a6b6214add842e803edcf0f457b0
Ourten <ourtencoop@gmail.com>
2016-10-29 01:25:34 +0200

    Add bases of an itemstack specialized GuiNode

---
+++ 1.7.10/src/main/java/fr/ourten/brokkgui/wrapper/container/ItemStackNodeBehavior.java
@@ -0,0 +1,14 @@
+package fr.ourten.brokkgui.wrapper.container;
+
+import fr.ourten.brokkgui.behavior.GuiBehaviorBase;
+
+/**
+ * @author Ourten 29 oct. 2016
+ */
+public class ItemStackNodeBehavior extends GuiBehaviorBase<ItemStackNode>
+{
+    public ItemStackNodeBehavior(final ItemStackNode model)
+    {
+        super(model);
+    }
+}
---
+++ 1.7.10/src/main/java/fr/ourten/brokkgui/wrapper/container/ItemStackNodeSkin.java
@@ -0,0 +1,14 @@
+package fr.ourten.brokkgui.wrapper.container;
+
+import fr.ourten.brokkgui.skin.GuiBehaviorSkinBase;
+
+/**
+ * @author Ourten 29 oct. 2016
+ */
+public class ItemStackNodeSkin extends GuiBehaviorSkinBase<ItemStackNode, ItemStackNodeBehavior>
+{
+    public ItemStackNodeSkin(final ItemStackNode model, final ItemStackNodeBehavior behavior)
+    {
+        super(model, behavior);
+    }
+}
