85b73a15a8cf64a1a16f855231e67bf105d354e7
Phenix246 <florian.gomes246@gmail.com>
2016-10-30 00:01:25 +0200

    Apply formatter

--- core/src/main/java/fr/ourten/brokkgui/element/GuiProgressBar.java
+++ core/src/main/java/fr/ourten/brokkgui/element/GuiProgressBar.java
@@ -13,106 +13,109 @@ import fr.ourten.teabeans.value.BaseProperty;
  */
 public class GuiProgressBar extends GuiLabeled
 {
-    private final BaseProperty<Float> progressBarProgressProperty;
-    private final BaseProperty<Background> progressBarColorProperty;
+    private final BaseProperty<Float>       progressBarProgressProperty;
+    private final BaseProperty<Background>  progressBarColorProperty;
     private final BaseProperty<EHAlignment> progressBarProgressDirection;
-    
-    public GuiProgressBar(final String text, final Background background, final float progressRatio, final EHAlignment progressDirection)
+
+    public GuiProgressBar(final String text, final Background background, final float progressRatio,
+            final EHAlignment progressDirection)
     {
         super(text);
         this.progressBarProgressProperty = new BaseProperty<>(0f, "progressBarProgressProperty");
         this.progressBarColorProperty = new BaseProperty<Background>(background, "progressBarColorProperty");
-        this.progressBarProgressDirection = new BaseProperty<EHAlignment>(progressDirection, "progressBarProgressDirection");
-        
-        this.progressBarProgressProperty.setChecker((old, set) -> {
-            if(set < 0 || set > 1)
+        this.progressBarProgressDirection = new BaseProperty<EHAlignment>(progressDirection,
+                "progressBarProgressDirection");
+
+        this.progressBarProgressProperty.setChecker((old, set) ->
+        {
+            if (set < 0 || set > 1)
                 return old;
             return set;
         });
-        
+
         this.progressBarProgressProperty.setValue(progressRatio);
-        
+
     }
-    
+
     public GuiProgressBar(final String text, final Background background, final float progressRatio)
     {
         this(text, background, progressRatio, EHAlignment.RIGHT);
     }
-    
+
     public GuiProgressBar(final Background background, final float progressRatio, final EHAlignment progressDirection)
     {
         this("", background, progressRatio, progressDirection);
     }
-    
+
     public GuiProgressBar(final Background background, final float progressRatio)
     {
         this(background, progressRatio, EHAlignment.RIGHT);
     }
-    
+
     public GuiProgressBar(final String text, final Background background)
     {
         this(text, background, 0);
     }
-    
+
     public GuiProgressBar(final float progressRatio, final EHAlignment progressDirection)
     {
         this(new Background(Color.WHITE), progressRatio, progressDirection);
     }
-    
+
     public GuiProgressBar(final Background background)
     {
         this(background, 0);
     }
-    
+
     public GuiProgressBar(final String text)
     {
         this(text, new Background(Color.WHITE));
     }
-    
+
     public GuiProgressBar()
     {
         this(new Background(Color.WHITE));
     }
-    
+
     @Override
     protected GuiProgressBarSkin<GuiProgressBar, GuiBehaviorBase<GuiProgressBar>> makeDefaultSkin()
     {
         return new GuiProgressBarSkin<>(this, new GuiBehaviorBase<>(this));
     }
-    
+
     public void setProgress(float progress)
     {
         this.getProgressProperty().setValue(progress);
     }
-    
+
     public BaseProperty<Float> getProgressProperty()
     {
         return this.progressBarProgressProperty;
     }
-    
+
     public BaseProperty<Background> getBackgroundProperty()
     {
         return this.progressBarColorProperty;
     }
-    
+
     public BaseProperty<EHAlignment> getProgressDirectionProperty()
     {
         return this.progressBarProgressDirection;
     }
-    
+
     public float getProgress()
     {
         return this.getProgressProperty().getValue();
     }
-    
+
     public Background getBackground()
     {
         return this.getBackgroundProperty().getValue();
     }
-    
+
     public EHAlignment getProgressDirection()
     {
         return this.getProgressDirectionProperty().getValue();
     }
-    
+
 }
--- core/src/main/java/fr/ourten/brokkgui/skin/GuiProgressBarSkin.java
+++ core/src/main/java/fr/ourten/brokkgui/skin/GuiProgressBarSkin.java
@@ -12,23 +12,23 @@ import fr.ourten.teabeans.binding.BaseBinding;
 public class GuiProgressBarSkin<C extends GuiProgressBar, B extends GuiBehaviorBase<C>> extends GuiLabeledSkinBase<C, B>
 {
     private final Rectangle progressBar;
-    
+
     public GuiProgressBarSkin(C model, B behaviour)
     {
         super(model, behaviour);
-        
+
         this.progressBar = new Rectangle(model.getxPos(), model.getyPos(), model.getWidth(), model.getHeight());
-        
+
         this.progressBar.getxPosProperty().bind(new BaseBinding<Float>()
         {
             {
                 super.bind(model.getxPosProperty());
             }
-            
+
             @Override
             public Float computeValue()
             {
-                switch(model.getProgressDirection())
+                switch (model.getProgressDirection())
                 {
                     case CENTER:
                         return model.getxPos() + ((model.getWidth() - progressBar.getWidth()) / 2);
@@ -39,29 +39,29 @@ public class GuiProgressBarSkin<C extends GuiProgressBar, B extends GuiBehaviorB
                 }
             }
         }
-        
+
         );
         this.progressBar.getyPosProperty().bind(model.getyPosProperty());
-        
+
         this.progressBar.getWidthProperty().bind(new BaseBinding<Float>()
         {
             {
                 super.bind(model.getProgressProperty());
             }
-            
+
             @Override
             public Float computeValue()
             {
                 return model.getWidth() * model.getProgress();
             }
         });
-        
+
         this.progressBar.getColorProperty().bind(new BaseBinding<Color>()
         {
             {
                 super.bind(model.getBackgroundProperty());
             }
-            
+
             @Override
             public Color computeValue()
             {
@@ -73,58 +73,58 @@ public class GuiProgressBarSkin<C extends GuiProgressBar, B extends GuiBehaviorB
             {
                 super.bind(model.getxPosProperty());
             }
-            
+
             @Override
             public Float computeValue()
             {
                 return model.getxPos() + (model.getWidth() / 2);
             }
-            
+
         });
         this.getText().getyPosProperty().bind(new BaseBinding<Float>()
         {
             {
                 super.bind(model.getyPosProperty());
             }
-            
+
             @Override
             public Float computeValue()
             {
                 return model.getyPos() + (model.getHeight() / 2);
             }
-            
+
         });
-        
+
         this.getText().getTextAlignmentProperty().setValue(EAlignment.MIDDLE_CENTER);
-        
+
         this.getText().getTextProperty().bind(new BaseBinding<String>()
         {
             {
                 super.bind(model.getProgressProperty());
             }
-            
+
             @Override
             public String computeValue()
             {
                 return model.getProgress() + "";
             }
         });
-        
+
         this.getText().getzLevelProperty().bind(new BaseBinding<Float>()
         {
             {
                 super.bind(model.getzLevelProperty());
             }
-            
+
             @Override
             public Float computeValue()
             {
                 return model.getzLevel() + 10;
             }
         });
-        
+
     }
-    
+
     @Override
     public void render(final EGuiRenderPass pass, final IGuiRenderer renderer, final int mouseX, final int mouseY)
     {
@@ -133,10 +133,10 @@ public class GuiProgressBarSkin<C extends GuiProgressBar, B extends GuiBehaviorB
         // XXX temp : zLevel not working
         this.getText().renderNode(renderer, pass, mouseX, mouseY);
     }
-    
+
     public Rectangle getProgressBar()
     {
         return this.progressBar;
     }
-    
+
 }
