a8e3cc4bf217df349862dfd6fa2e825a77069ed3
Unknown <allocompte@hotmail.fr>
2018-02-21 12:38:05 +0100

    Add CTRL effect on delete and cursor movement
    CTRL + BACK deletes the word before the cursor
    CTRL + SUPPR deletes the word after the cursor
    CTRL + LEFT moves the cursor just before the previous word
    CTRL + RIGHT moves the cursor just after the next word

--- core/src/main/java/org/yggard/brokkgui/behavior/GuiTextfieldBehavior.java
+++ core/src/main/java/org/yggard/brokkgui/behavior/GuiTextfieldBehavior.java
@@ -1,5 +1,7 @@
 package org.yggard.brokkgui.behavior;
 
+import java.util.regex.Pattern;
+
 import org.yggard.brokkgui.BrokkGuiPlatform;
 import org.yggard.brokkgui.element.GuiTextfield;
 import org.yggard.brokkgui.event.CursorMoveEvent;
@@ -28,17 +30,33 @@ public class GuiTextfieldBehavior<T extends GuiTextfield> extends GuiBehaviorBas
         if (event.getKey() == this.keyboard.getKeyCode("DELETE"))
         {
             if (this.getModel().isEditable())
-                contentChanged = this.deleteAfterCursor();
+            {
+                if(keyboard.isCtrlKeyDown())
+                    contentChanged = this.deleteWordAfterCursor();
+                else
+                    contentChanged = this.deleteAfterCursor();
+            }
         }
         else if (event.getKey() == this.keyboard.getKeyCode("BACK"))
         {
             if (this.getModel().isEditable())
-                contentChanged = this.deleteFromCursor();
+            {
+                if(keyboard.isCtrlKeyDown())
+                    contentChanged = this.deleteWordBeforeCursor();
+                else
+                    contentChanged = this.deleteFromCursor();
+            }
         }
         else if (event.getKey() == this.keyboard.getKeyCode("LEFT"))
-            this.setCursorPosition(this.getModel().getCursorPosition() - 1);
+            if(keyboard.isCtrlKeyDown())
+                this.setCursorPosition(this.previousWordPosition());
+            else
+                this.setCursorPosition(this.getModel().getCursorPosition() - 1);
         else if (event.getKey() == this.keyboard.getKeyCode("RIGHT"))
-            this.setCursorPosition(this.getModel().getCursorPosition() + 1);
+            if(keyboard.isCtrlKeyDown())
+                this.setCursorPosition(this.nextWordPosition());
+            else
+                this.setCursorPosition(this.getModel().getCursorPosition() + 1);
         else if (event.getKey() == this.keyboard.getKeyCode("V")
                 && BrokkGuiPlatform.getInstance().getKeyboardUtil().isCtrlKeyDown())
         {
@@ -129,4 +147,95 @@ public class GuiTextfieldBehavior<T extends GuiTextfield> extends GuiBehaviorBas
             this.getModel().setCursorPosition(cursorPosition);
         }
     }
+    
+    private static final Pattern alphaNumRegex = Pattern.compile("[a-zA-Z0-9]");
+    
+    protected int previousWordPosition() 
+    {
+        String textBeforeCursor = getModel().getText().substring(0, getModel().getCursorPosition());
+        if(textBeforeCursor.isEmpty())
+            return 0;
+        int pos = getModel().getCursorPosition();
+        
+        boolean foundCharacter = false;
+        while(pos > 0) 
+        {
+            String charAtPos = textBeforeCursor.charAt(pos - 1) + "";
+            if(alphaNumRegex.matcher(charAtPos).matches()) 
+            {
+                foundCharacter = true;
+                pos--;
+            }
+            else if(charAtPos.equals(" ")) 
+            {
+                if(foundCharacter)
+                    break;
+                else
+                    pos--;
+            }
+            else if(pos == getModel().getCursorPosition()) 
+            {
+                pos--;
+                break;
+            } 
+            else 
+            {
+                pos--;
+            }
+        }
+        
+        return pos;
+    }
+    
+    protected int nextWordPosition() {
+        if(getModel().getCursorPosition() == getModel().getText().length())
+            return getModel().getCursorPosition();
+        String text = getModel().getText();
+        int pos = getModel().getCursorPosition();
+        boolean foundSpace = false;
+        while(pos < getModel().getText().length())
+        {
+            String charAtPos = text.charAt(pos) + "";
+            if(charAtPos.equals(" ")) 
+            {
+                foundSpace = true;
+                pos++;
+            }
+            else if(foundSpace) 
+            {
+                break;
+            } 
+            else if(!alphaNumRegex.matcher(charAtPos).matches() && pos != getModel().getCursorPosition())
+            {
+                break;
+            }
+            else
+            {
+                pos++;
+            }
+        }
+        return pos;
+    }
+    
+    protected boolean deleteWordBeforeCursor() 
+    {
+        if(this.getModel().getCursorPosition() == 0)
+            return false;
+        int pos = previousWordPosition();
+        String currentText = this.getModel().getText();
+        this.getModel().setText(currentText.substring(0, pos) + currentText.substring(this.getModel().getCursorPosition()));
+        this.setCursorPosition(pos);
+        return true;
+    }
+    
+    protected boolean deleteWordAfterCursor()
+    {
+        if(this.getModel().getCursorPosition() == this.getModel().getText().length())
+            return false;
+        int pos = nextWordPosition();
+        String currentText = this.getModel().getText();
+        this.getModel().setText(currentText.substring(0, getModel().getCursorPosition()) + currentText.substring(pos));
+        return true;
+    }
+    
 }
