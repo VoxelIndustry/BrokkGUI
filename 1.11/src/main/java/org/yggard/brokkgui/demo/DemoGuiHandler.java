f0301ec51e5eecae2613f8fdd3c47e477bd20b9e
Charles Javerliat <charlesjaverliat@gmail.com>
2018-08-27 22:20:52 +0200

    Added 1.11 Support (#11)

---
+++ 1.11/src/main/java/org/yggard/brokkgui/demo/DemoGuiHandler.java
@@ -0,0 +1,31 @@
+package org.yggard.brokkgui.demo;
+
+import org.yggard.brokkgui.wrapper.impl.BrokkGuiManager;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.network.IGuiHandler;
+
+/**
+ * @author Ourten 31 oct. 2016
+ */
+public class DemoGuiHandler implements IGuiHandler
+{
+    @Override
+    public Object getServerGuiElement(final int ID, final EntityPlayer player, final World world, final int x,
+            final int y, final int z)
+    {
+        if (ID == 0)
+            return new ContainerDemo(player.inventory);
+        return null;
+    }
+
+    @Override
+    public Object getClientGuiElement(final int ID, final EntityPlayer player, final World world, final int x,
+            final int y, final int z)
+    {
+        if (ID == 0)
+            return BrokkGuiManager.getBrokkGuiContainer(new GuiContainerDemo(new ContainerDemo(player.inventory)));
+        return null;
+    }
+}
---
+++ 1.11/src/main/java/org/yggard/brokkgui/demo/GuiContainerDemo.java
@@ -0,0 +1,52 @@
+package org.yggard.brokkgui.demo;
+
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fluids.FluidStack;
+import org.yggard.brokkgui.paint.Color;
+import org.yggard.brokkgui.paint.Texture;
+import org.yggard.brokkgui.panel.GuiAbsolutePane;
+import org.yggard.brokkgui.wrapper.container.BrokkGuiContainer;
+import org.yggard.brokkgui.wrapper.elements.FluidStackView;
+import org.yggard.brokkgui.wrapper.elements.ItemStackView;
+
+/**
+ * @author Ourten 31 oct. 2016
+ */
+public class GuiContainerDemo extends BrokkGuiContainer<ContainerDemo>
+{
+    private static final int xSize = 176, ySize = 200;
+
+    private static final Texture BACKGROUND = new Texture("brokkguidemo:textures/gui/container_background.png", 0, 0,
+            GuiContainerDemo.xSize / 256.0f, GuiContainerDemo.ySize / 256.0f);
+
+    public GuiContainerDemo(final ContainerDemo container)
+    {
+        super(container);
+
+        this.setWidth(GuiContainerDemo.xSize);
+        this.setHeight(GuiContainerDemo.ySize);
+        this.setxRelativePos(0.5f);
+        this.setyRelativePos(0.5f);
+
+        final GuiAbsolutePane mainPanel = new GuiAbsolutePane();
+        this.setMainPanel(mainPanel);
+
+        final ItemStackView view = new ItemStackView(new ItemStack(Items.APPLE));
+        view.setItemTooltip(true);
+        view.setWidth(18);
+        view.setHeight(18);
+        view.setColor(new Color(1, 1, 1, 0.5f));
+        mainPanel.addChild(view);
+
+        FluidStackView fluidStackView = new FluidStackView(new FluidStack(FluidRegistry.WATER, 1000));
+        fluidStackView.setWidth(18);
+        fluidStackView.setHeight(64);
+        fluidStackView.setFlowing(true);
+
+        mainPanel.addChild(fluidStackView, 4, 4);
+
+        mainPanel.setBackgroundTexture(GuiContainerDemo.BACKGROUND);
+    }
+}
