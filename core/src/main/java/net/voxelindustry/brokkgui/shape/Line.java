9341fae0668fccf8a5b24618242212a294ba3fe9
Ourten <ourtencoop@gmail.com>
2018-12-26 17:30:04 +0100

    Fix Line width and height using end point instead of magnitude

--- core/src/main/java/net/voxelindustry/brokkgui/shape/Line.java
+++ core/src/main/java/net/voxelindustry/brokkgui/shape/Line.java
@@ -5,17 +5,17 @@ import net.voxelindustry.brokkgui.paint.RenderPass;
 
 public class Line extends GuiShape
 {
-    public Line(final float startX, final float startY, final float endX, final float endY)
+    public Line(float startX, float startY, float endX, float endY)
     {
         super("line");
         this.setxTranslate(startX);
         this.setyTranslate(startY);
 
-        this.setWidth(endX);
-        this.setHeight(endY);
+        this.setWidth(Math.abs(startX - endX));
+        this.setHeight(Math.abs(startY - endY));
     }
 
-    public Line(final float endX, final float endY)
+    public Line(float endX, float endY)
     {
         this(0, 0, endX, endY);
     }
@@ -26,7 +26,7 @@ public class Line extends GuiShape
     }
 
     @Override
-    public void renderContent(final IGuiRenderer renderer, final RenderPass pass, final int mouseX, final int mouseY)
+    public void renderContent(IGuiRenderer renderer, RenderPass pass, int mouseX, int mouseY)
     {
         if (pass == RenderPass.MAIN)
             renderer.getHelper().drawColoredLine(renderer, this.getxPos() + this.getxTranslate(),
