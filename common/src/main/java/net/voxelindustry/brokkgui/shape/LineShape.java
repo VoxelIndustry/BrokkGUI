278de741765ba1039f71f7224efd5b8b20d7a951
unknown <allocompte@hotmail.fr>
2019-03-07 20:48:40 +0100

    Implement LineShape#isMouseInside(GuiShape, int, int)

--- common/src/main/java/net/voxelindustry/brokkgui/shape/LineShape.java
+++ common/src/main/java/net/voxelindustry/brokkgui/shape/LineShape.java
@@ -37,13 +37,33 @@ public class LineShape implements ShapeDefinition
     public void drawTextured(GuiShape shape, IGuiRenderer renderer, float startX, float startY, Texture texture,
                              float zLevel)
     {
-        // TODO : Add helper for drawing textured lines
+
     }
 
     @Override
     public boolean isMouseInside(GuiShape shape, int mouseX, int mouseY)
     {
-        // TODO : Impl a check for non-axis-aligned boxes
-        return false;
+        if(!Rectangle.SHAPE.isMouseInside(shape, mouseX, mouseY)) // Bad behavior expected with very high line width
+            return false;                                         // and at line's extremities
+
+        /*
+            Let AM = (mouseX - startX, mouseY - startY) a vec2
+            Let AB = (shapeWidth, shapeHeight) a vec2
+            dist(M, AB) = |det(AB,AM)| / ||AB||
+        */
+
+        // Vector from segment beginning to mouse position
+        float AMx = mouseX - (shape.getxPos() + shape.getxTranslate());
+        float AMy = mouseY - (shape.getyPos() + shape.getyTranslate());
+
+        float ABx = shape.getWidth();
+        float ABy = shape.getHeight();
+
+        // Remember : det(A,B) = Ax * By - Ay * Bx
+
+        double dist = Math.abs(AMx * ABy - AMy * ABx) / Math.sqrt(ABx * ABx + ABy * ABy);
+
+        return dist < lineWidthSupplier.get() / 4d;
     }
+
 }
