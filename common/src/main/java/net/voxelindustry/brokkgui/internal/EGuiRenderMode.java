8fdc2522c0f645e5aef684b4107f60ecbeb3fd79
MFFbrokenSwing <allocompte@hotmail.fr>
2019-03-07 18:36:07 +0100

    Render (#14)
    * Add TRIANGLES_FAN render mode
    
    * Add others draw modes supported by OpenGL

--- common/src/main/java/net/voxelindustry/brokkgui/internal/EGuiRenderMode.java
+++ common/src/main/java/net/voxelindustry/brokkgui/internal/EGuiRenderMode.java
@@ -5,5 +5,5 @@ package net.voxelindustry.brokkgui.internal;
  */
 public enum EGuiRenderMode
 {
-    POINTS, LINE_STRIP, LINES, QUADS, TRIANGLES, TRIANGLES_STRIP
+    POINTS, LINE_STRIP, LINES, QUADS, TRIANGLES, TRIANGLE_STRIP, TRIANGLE_FAN, POLYGON, LINE_LOOP, QUAD_STRIP
 }
