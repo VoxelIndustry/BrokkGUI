9b3b272b8b63148ce59b0240c628a4a46ceb8776
Ourten <ourtencoop@gmail.com>
2019-07-23 21:34:11 +0200

    Remove skin related classes

--- common/src/main/java/net/voxelindustry/brokkgui/skin/IGuiSkin.java
+++
@@ -1,9 +0,0 @@
-package net.voxelindustry.brokkgui.skin;
-
-import net.voxelindustry.brokkgui.internal.IGuiRenderer;
-import net.voxelindustry.brokkgui.paint.RenderPass;
-
-public interface IGuiSkin
-{
-    void render(final RenderPass pass, final IGuiRenderer renderer, int mouseX, int mouseY);
-}
