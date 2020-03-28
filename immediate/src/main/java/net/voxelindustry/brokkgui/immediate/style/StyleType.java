ddb792a51325ac222c8c0e91374c6fb0787478c4
Ourten <ourtencoop@gmail.com>
2020-03-28 14:18:03 +0100

    Make StyleType an object

--- immediate/src/main/java/net/voxelindustry/brokkgui/immediate/style/StyleType.java
+++ immediate/src/main/java/net/voxelindustry/brokkgui/immediate/style/StyleType.java
@@ -1,8 +1,20 @@
 package net.voxelindustry.brokkgui.immediate.style;
 
-public enum StyleType
+public class StyleType
 {
-    LIGHT,
-    NORMAL,
-    DARK
+    public static final StyleType LIGHT  = new StyleType("light");
+    public static final StyleType NORMAL = new StyleType("normal");
+    public static final StyleType DARK   = new StyleType("dark");
+
+    private String name;
+
+    public StyleType(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
 }
